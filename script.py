d2d388106deee2a1deaab6bcf93788b7cc35593d
Agust Fricke <agustfricke@protonmail.com>
2023-03-29 02:16:34 -0300

    fix delete 204  response

--- script.py
+++ script.py
@@ -6,16 +6,23 @@ def send_request(method, endpoint, data=None):
     url = BASE_URL + endpoint
     try:
         response = getattr(requests, method.lower())(url, json=data)
-        response.raise_for_status()
-        return response.json()
+        if response.content:  # check if response is not empty
+            response.raise_for_status()
+            return response.json()
+        elif response.status_code == 204:
+            # return success message with ID from the URL
+            id_ = endpoint.split('/')[-2]
+            return {'message': f'Deleted object with ID: {id_}'}
+        else:
+            raise Exception(f"An unknown error occurred. Response content: {response.content}")
     except requests.exceptions.HTTPError as errh:
-        raise Exception(f"An HTTP Error occurred: {errh}")
+        print(f"An HTTP Error occurred: {errh}")
     except requests.exceptions.ConnectionError as errc:
-        raise Exception(f"An Error Connecting to the API occurred: {errc}")
+        print(f"An Error Connecting to the API occurred: {errc}")
     except requests.exceptions.Timeout as errt:
-        raise Exception(f"A Timeout Error occurred: {errt}")
+        print(f"A Timeout Error occurred: {errt}")
     except requests.exceptions.RequestException as err:
-        raise Exception(f"An Unknown Error occurred: {err}")
+        print(f"An Unknown Error occurred: {err}")
 
 if __name__ == '__main__':
     method = input('Select HTTP method (GET, POST, PUT, DELETE): ').upper()
@@ -32,12 +39,6 @@ if __name__ == '__main__':
     else:
         data = None
 
-    try:
-        response = send_request(method, endpoint, data)
-        if method == 'DELETE':
-            id_ = response.get('id')
-            print(f'Deleted object with ID: {id_}')
-        else:
-            print(response)
-    except Exception as e:
-        print(e)
+    response = send_request(method, endpoint, data)
+    if response:
+        print(response)
