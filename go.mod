9610a098c998e8da9c353f6026cb4f146318d151
Agust Fricke <agustfricke@protonmail.com>
2023-05-19 07:44:23 -0300

    file upload

--- go.mod
+++ go.mod
@@ -2,3 +2,9 @@ module github.com/agustfricke/snet-client-api
 
 go 1.20
 
+require (
+	github.com/buger/goterm v1.0.4 // indirect
+	github.com/gookit/color v1.5.3 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,59 @@ package main
 
 import (
 	"bufio"
-  "mime/multipart"
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/user"
+	"strconv"
 	"strings"
+	"time"
+
+  "github.com/gookit/color"
 )
 
+func getTerminalSize() (int, int, error) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var width, height int
+	fmt.Sscanf(string(out), "%d %d", &height, &width)
+
+	return width, height, nil
+}
+
 func main() {
+
+	// Clear the terminal
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+	// Resize the terminal to full size
+	resizeCmd := exec.Command("printf", "\033[8;0;0t")
+	resizeCmd.Stdout = os.Stdout
+	resizeCmd.Run()
+
+	// Print content to fill the terminal
+	width, height, _ := getTerminalSize()
+
+	// Restore the terminal to its original size
+	restoreCmd := exec.Command("printf", "\033[8;"+strconv.Itoa(height)+";"+strconv.Itoa(width)+"t")
+	restoreCmd.Stdout = os.Stdout
+	restoreCmd.Run()
+
   var url string
   reader := bufio.NewReader(os.Stdin)
-
 
   for {
     if (url == "") {
@@ -27,7 +65,13 @@ func main() {
         return
       }
 
-      fmt.Println("Hello", currentUser.Username, "welcome to snet client api!")
+      text := "Hello, " + currentUser.Username + "! Welcome to snet client api!\n"
+
+      for _, char := range text {
+        color.Cyan.Printf(string(char))
+        time.Sleep(50 * time.Millisecond) 
+      }
+
       fmt.Print("Enter a base URL to your session: ")
       fmt.Scanf("%v\n", &url)
     }
@@ -73,32 +117,52 @@ func main() {
 
       // POST
       if method == "1" {
+        // Verificar si se debe enviar un archivo
+        var sendFile string
+        fmt.Print("Do you want to send a file? (y/n): ")
+        fmt.Scanf("%s\n", &sendFile)
+        sendFile = strings.ToLower(strings.TrimSpace(sendFile))
 
-        req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+        if sendFile == "y" {
+          // Obtener el path del archivo
+          var filePath string
+          fmt.Print("Enter the file path: ")
+          fmt.Scanf("%s\n", &filePath)
+        // Realizar la solicitud POST
+        err = sendPostRequest(url, jsonData, filePath)
+
         if err != nil {
-          fmt.Println("Error al crear la solicitud PUT:", err)
+          fmt.Println("Error sending POST request:", err)
           return
         }
 
-        req.Header.Set("Content-Type", "application/json")
-        req.Header.Set("Authorization", "Bearer "+ token)
+        } else {
+          req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+          if err != nil {
+            fmt.Println("Error al crear la solicitud POST:", err)
+            return
+          }
 
-        client := &http.Client{}
-        resp, err := client.Do(req)
-        if err != nil {
-          fmt.Println("Error al realizar la solicitud PUT:", err)
-          return
+          req.Header.Set("Content-Type", "application/json")
+          req.Header.Set("Authorization", "Bearer "+ token)
+
+          client := &http.Client{}
+          resp, err := client.Do(req)
+          if err != nil {
+            fmt.Println("Error al realizar la solicitud PUT:", err)
+            return
+          }
+          defer resp.Body.Close()
+
+          respBody, err := ioutil.ReadAll(resp.Body)
+          if err != nil {
+            fmt.Println("Error al leer la respuesta:", err)
+            return
+          }
+
+          fmt.Println("Respuesta del servidor:")
+          fmt.Println(string(respBody))
         }
-        defer resp.Body.Close()
-
-        respBody, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-          fmt.Println("Error al leer la respuesta:", err)
-          return
-        }
-
-        fmt.Println("Respuesta del servidor:")
-        fmt.Println(string(respBody))
 
 
       // PUT
@@ -208,3 +272,77 @@ func main() {
 }
 
 
+// Función para enviar una solicitud POST con la opción de archivo adjunto
+func sendPostRequest(url string, jsonData []byte, filePath string) error {
+  // Crea un cuerpo de solicitud con formulario multipart
+  body := &bytes.Buffer{}
+  writer := multipart.NewWriter(body)
+
+  // Agrega los datos JSON al formulario multipart
+  jsonPart, err := writer.CreateFormField("json")
+  if err != nil {
+    return err
+  }
+  _, err = io.Copy(jsonPart, bytes.NewReader(jsonData))
+  if err != nil {
+    return err
+  }
+
+  // Verifica si se debe adjuntar un archivo
+  if filePath != "" {
+    // Abre el archivo
+    file, err := os.Open(filePath)
+    if err != nil {
+      return err
+    }
+    defer file.Close()
+
+    // Crea una parte para el archivo en el formulario multipart
+    filePart, err := writer.CreateFormFile("file", filePath)
+    if err != nil {
+      return err
+    }
+
+    // Copia el contenido del archivo a la parte del formulario
+    _, err = io.Copy(filePart, file)
+    if err != nil {
+      return err
+    }
+  }
+
+  // Finaliza el formulario multipart
+  err = writer.Close()
+  if err != nil {
+    return err
+  }
+
+  // Crea una solicitud POST
+  req, err := http.NewRequest(http.MethodPost, url, body)
+  if err != nil {
+    return err
+  }
+  defer req.Body.Close()
+
+  // Establece el encabezado Content-Type adecuado
+  req.Header.Set("Content-Type", writer.FormDataContentType())
+
+  // Realiza la solicitud POST
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
+
+  // Leer la respuesta del servidor
+  respBody, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return err
+  }
+
+  // Imprimir la respuesta del servidor
+  fmt.Println("Respuesta del servidor:")
+  fmt.Println(string(respBody))
+
+  return nil
+}
